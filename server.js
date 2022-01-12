2e4f7e0729202ea19ebcf1eaa7539c7d0790e533
Opilane TTHK <opilane@tthk.ee>
2022-01-12 12:23:09 +0200

    Server.js

--- server.js
+++ server.js
@@ -12,6 +12,90 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
+//initialize session
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect to the db
+mongoose.connect('mongodb://localhost:27017/mySuperSecretDB', {useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model('User', userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', (req, res) => {
+    User.register({username: req.body.username}, req.body.password, (error, user) => {
+        if(error) {
+            console.log(error);
+            res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.render('secrets');
+            });
+        }
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, (error) =>{
+        if(error){
+            console.log(error)
+            res.redirect('/login');
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets');
+            });
+        }
+    })
+});
+
+app.get('/secrets', (req, res) => {
+    if(req.isAuthenticated()){
+        res.render('secrets')
+    } else {
+        res.redirect('/')
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+});
+//heheheha
+
 
 const port = 3000;
 
